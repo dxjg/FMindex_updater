e4dc836c61f26a23a96917f068fa6aef40141417
Craig Hennessy <chennes3@gmail.com>
2015-12-07 19:30:29 -0500

    Added delBase function. Discovered bug in both

--- project_code/fmindex.py
+++ project_code/fmindex.py
@@ -17,14 +17,24 @@ class FMindex(object):
         Occ = self.occ[self.bwt[index]][index]
         return C + Occ - 1
 
-    def _insertIntoBWT(self, i, c):
-        overwritten, k = self._stageIb(i, c)
-        self._stageIIa(overwritten, i)
-        self._stageIIb(i, k)
+    def insBase(self, i, c):
+        overwritten, k = self.ins_stageIb(i, c)
+        self.ins_stageIIa(overwritten, k, i)
+        self.ins_stageIIb(i, k)
 
-    #Find the row corresponding to the ith rotation
-    def _index(self, i):
-        return self.sa.index(i)
+    def delBase(self, i):
+        k = self.del_stageIb(i)
+        self.del_stageIIa(i)
+        self.del_stageIIb(i, k)
+
+    def printBWT(self):
+        for i in range(len(self.bwt)):
+            print self.bwt[i]
+
+
+################################################################################
+#                           "Private" Methods Below                            #
+################################################################################
 
 
     def _createBWM(self, t):
@@ -45,12 +55,23 @@ class FMindex(object):
             sa.append(indexSA[row])
         return sa
     
+    #Find the row corresponding to the ith rotation
+    def _index(self, i):
+        return self.sa.index(i)
+
     #See paper on dynamic suffix arrays for this algorithm
     def _updateSA(self, k_prime, i):
-        for j in range(len(self.sa)):
-            if self.sa[j] >= i:
-                self.sa[j] += 1
-        self.sa.insert(k_prime, i)
+        if len(self.sa) < len(self.bwt):
+            for j in range(len(self.sa)):
+                if self.sa[j] >= i:
+                    self.sa[j] += 1
+            self.sa.insert(k_prime, i)
+        else:
+            for j in range(len(self.sa)):
+                if self.sa[j] >= i:
+                    self.sa[j] -= 1
+            del self.sa[k_prime]
+
 
     #from the python fmindex github. TODO link here
     def _createCount(self):
@@ -85,6 +106,7 @@ class FMindex(object):
                     table[i][j] = prev
         return table
 
+    #Part of insBase, subBase, and delBase
     def _move(self, j, j_prime):
         if j < j_prime:
             print self.bwt[:j], self.bwt[j+1:j_prime+1], self.bwt[j], self.bwt[j_prime + 1:]
@@ -93,57 +115,108 @@ class FMindex(object):
             print self.bwt[:j_prime], self.bwt[j], self.bwt[j_prime:j], self.bwt[j:]
             temp = self.bwt[:j_prime] + self.bwt[j] + self.bwt[j_prime:j] + self.bwt[j:]
         self.bwt = temp
+        print "Moving rows" , j, j_prime
         tempSA = self.sa.pop(j)
+        print self.sa, tempSA
         self.sa.insert(j_prime, tempSA)
         return 
 
+####################################################################################
+#                              Insertion of 1 element                              #
+####################################################################################
+
     '''Stage Ib. Given the insertion index i and the character to insert c,
        Insert c into the bwt at i, overwriting the character there'''
-    def _stageIb(self, i, c):
+    def ins_stageIb(self, i, c):
         #TODO right now count is rebuilt entirely (time consuming)
         #Waiting on implementing navarro
         k = self.sa.index(i)
         overwritten = self.bwt[k]
         overwritten_k = self._lf(k)
-        print k, overwritten_k
+        print "INS: Beginning stage Ib"
+        print "INS: Overwritten index:", k, "; LF of Overwritten index:", overwritten_k
         self.bwt = self.bwt[:k] + c + self.bwt[k+1:]
-        print "After stage Ib:"
-        self._printBWT()
+        print "INS: After stage Ib:", self.bwt
+        self.count = self._createCount()
+        self.occ = self._createOcc()
         return overwritten, overwritten_k
 
-    def _stageIIa(self, overwritten, i):
-        k = self.sa.index(i)
-        insertPoint = self._lf(k) + 1
-        print "inserting T at row ", insertPoint, i, k
-        temp = self.bwt[:insertPoint] + overwritten + self.bwt[insertPoint:]
+    def ins_stageIIa(self, overwritten, k, i): 
+        print "inserting T at row:", k
+        temp = self.bwt[:k] + overwritten + self.bwt[k:]
         self.bwt = temp
-        print "After stage IIa", self.bwt, len(self.bwt)
-        self._printBWT()
+        print "INS: After stage IIa", self.bwt
         self.count = self._createCount()
         print self.count
-        self._updateSA(insertPoint, i)
+        self._updateSA(k, i)
         self.occ = self._createOcc()
 
-    def _stageIIb(self, i, k):
+    def ins_stageIIb(self, i, k):
+        j = self.sa.index(i-1)
+        j_prime = self._lf(k)
+        while j!= j_prime:
+            print "INS: j= ", j, ", j' = ", j_prime
+            new_j = self._lf(j)
+            self._move(j, j_prime)
+            print "INS: Loop of Stage IIb", self.bwt
+            j = new_j
+            j_prime = self._lf(j_prime)
+            print j, j_prime
+        print "INS: After Stage IIb", self.bwt
+
+
+####################################################################################
+#                              Deletion of 1 element                               #
+####################################################################################
+        
+
+    '''Stage Ib. Given the deletion index i, overwrite the character at Ti with Ti-1'''
+    def  del_stageIb(self, i):
+        #TODO right now count is rebuilt entirely (time consuming)
+        #Waiting on implementing navarro
+        T = "CTGCTGC$"
+        print T[i], T[i-1]
+        k = self.sa.index(i)
+        overwritten = self.bwt[k]
+        j = self._lf(k)
+        print k, j, self.bwt[k], self.bwt[j]
+        overwritten_k = self._lf(k)
+        print i, k, overwritten_k, j, self.sa[k], self.sa[overwritten_k], self.sa[j]
+        print "Should be T:", self.bwt[k]
+        c = self.bwt[k]
+        print "DEL: Beginning stage Ib"
+        print "DEL: Overwritten index:", k, "; LF of Overwritten index:", overwritten_k
+        self.bwt = self.bwt[:k] + self.bwt[j] + self.bwt[k+1:]
+        print "DEL: After stage Ib:"
+        self.printBWT()
+        return j
+
+    def del_stageIIa(self, i):
+        k = self.sa.index(i)
+        F = sorted(self.bwt)
+        print "Want to delete row:", F[k], ":", self.bwt[k]
+        deletionPoint = k
+        print "DEL: Deleting at row:", deletionPoint
+        temp = self.bwt[:deletionPoint] + self.bwt[deletionPoint + 1:]
+        self.bwt = temp
+        print "DEL: After stage IIa", self.bwt, len(self.bwt)
+        self.printBWT()
+        self.count = self._createCount()
+        print self.count
+        self._updateSA(deletionPoint, i)
+        self.occ = self._createOcc()
+
+    def del_stageIIb(self, i, k):
         j = k + 1
         j_prime = self._lf(k)
         while j!= j_prime:
-            print "j= ", j, ", j' = ", j_prime
+            print "DEL: j= ", j, ", j' = ", j_prime
             new_j = self._lf(j)
             self._move(j, j_prime)
-            print "One loop of Stage IIb", self.bwt
+            print "DEL: Loop of Stage IIb", self.bwt
             j = new_j
             j_prime = self._lf(j_prime)
-        print "After Stage IIb"
-        self._printBWT()
-
+        print "DEL: After Stage IIb"
+        self.printBWT()
         
-    def _printBWT(self):
-        for i in range(len(self.bwt)):
-            print self.bwt[i]
-
-        
-
-        
-
 
--- project_code/test_fm.py
+++ project_code/test_fm.py
@@ -9,23 +9,39 @@ import fmindex
 
 
 def main():
-    string = "CTCTGC"
+    #Genome from paper is: CTCTGC 
+    string = sys.argv[1]
+    if '$' in string:
+        print "Don't include $ in genome"
+        sys.exit(0)
     fm = fmindex.createFM(string)
-    print string, fm.bwt
     import pprint
-    print "Count array"
-    pprint.pprint(fm.count)
-    print    "occurance array"
-    for c in sorted(fm.occ.keys()):
-        result = c + ": "
-        for i in range(len(fm.bwt)):
-            result +=  str(fm.occ[c][i]) + ", "
-        print result
-    print "suffix array indexes"
+    index = int(sys.argv[2])
+    insert = sys.argv[3]
+    print "Before", fm.bwt
+    afterString = string[:index] + insert + string[index:]
+    afterfm = fmindex.FMindex(afterString)
+    testIns(fm, index, insert)
+    print "Expected result:", afterfm.bwt
+    print "Actual results: ", fm.bwt
+    #testDel(fm, index)
+
+def testIns(fm, index, insert):
+    print "Inserting", insert, "at index", index
+    before = fm.bwt
+    fm.insBase(index, insert)        
+    print "After insertion"
+    after = fm.bwt
     for i in range(len(fm.bwt)):
         print fm.sa[i], sorted(fm.bwt)[i], fm.bwt[i]
-    fm._insertIntoBWT(2, "G")        
-    
+
+def testDel(fm, index):
+    print "Deleting 'G' from index 2" 
+    print fm.bwt
+    fm.delBase(index)
+    print "After deletion"
+    for i in range(len(fm.bwt)):
+        print fm.sa[i], sorted(fm.bwt)[i], fm.bwt[i]
 
 if __name__ == '__main__':
     main()
