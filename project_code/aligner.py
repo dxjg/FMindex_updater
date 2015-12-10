889e853e7a6a61747e96b405846a3c092437300d
Gwen <ghoffm12@jhu.edu>
2015-12-09 22:40:32 -0500

    Aligner works for multiple edits! Some edit lists may still be buggy

--- project_code/aligner.py
+++ project_code/aligner.py
@@ -47,74 +47,83 @@ class Aligner(object):
 	def _edit_str(self, edit_list, word, read):
 		#TODO
 		edits = []
-		for x in range(0, len(read)):
+		for x in range(0, max(len(read), len(word))):
 			if x in edit_list:
 				edits.append(edit_list[x])
 			else:
-				edits.append(read[x])
+				if x < len(read) and x < len(word):
+					edits.append(read[x])
 		return edits
 
-	def _findNeighbor(self, wlist, read, word, d, n, position, edit_list):
+	def _findNeighbor(self, wlist, read, word, d, n, position, edit_list, elist):
 		letters = ['A', 'C', 'G', 'T']
 		for x in letters:
 			word2 = word[0:position] + x + word[position + 1:] # create substitutions
 			edit_list2 = copy.deepcopy(edit_list)
 			if word2 not in wlist:
 				wlist.append(word2)
-				pos = self._query_fm(word2)
 				edit_list2[position] = x
- 				if pos > -1:
- 					#print pos, word2, "R"
-					return pos, word2, edit_list2
-			if (n <= d):
-				#print n
-				return self._findNeighbor(wlist, read, word2, d, n + 1, 0, edit_list2)
-
+				elist.append(edit_list2)
+			#if (n < d):
+			#	self._findNeighbor(wlist, read, word2, d, n + 1, 0, edit_list, elist)
+				
 			word3 = word[0:position] + word[position + 1:] # create deletions
 			edit_list3 = copy.deepcopy(edit_list)
 			if word3 not in wlist:
 				wlist.append(word3)
-				pos = self._query_fm(word3)
 				edit_list3[position] = None
-				if pos > -1:
-					#edit_list[position] = "D"
-					#print pos, word3, "D"
-					return pos, word3, edit_list3
-			if (n <= d):
-				return self._findNeighbor(wlist, read, word3, d, n + 1, 0, edit_list3)
+				elist.append(edit_list3)
+			#if (n < d):
+			#	self._findNeighbor(wlist, read, word3, d, n + 1, 0, edit_list, elist)
 
 			word4 = word[0:position] + x + word[position:] # create insertions
 			edit_list4 = copy.deepcopy(edit_list)
 			if word4 not in wlist:
 				wlist.append(word4)
 				#print word4
-				pos = self._query_fm(word4)
 				edit_list4[position] = x + word[position]
+				elist.append(edit_list4)
+			#if (n < d):
+			#	self._findNeighbor(wlist, read, word4, d, n + 1, 0, edit_list, elist)
+		#if (position <= len(word)):
+		#	self._findNeighbor(wlist, read, read, d, n, position + 1, edit_list, elist)		
+
+	def _neighbor_control(self, read):
+		wlist = []
+		wlist.append(read)
+		edit_list = {}
+		elist = []
+		elist.append({})
+		n = 1
+		pos = -1
+		word = read
+		ind1 = 0
+		while n < len(read) and pos < 0:
+			ind2 = len(wlist)
+			for i in range(ind1, len(wlist)):
+				for p in range(0, len(wlist[i])):
+					self._findNeighbor(wlist, read, wlist[i], 1, 1, p, elist[i], elist)
+			ind1 = ind2
+			for x in range(ind2, len(wlist)):
+				pos = self._query_fm(wlist[x])
 				if pos > -1:
-					#edit_list[position] = "I"
-					#print pos, word4, "I"
-					return pos, word4, edit_list4
-			if (n <= d):
-				return self._findNeighbor(wlist, read, word4, d, n + 1, 0, edit_list4)
-		if (position < len(word)):
-			return self._findNeighbor(wlist, read, read, d, n, position + 1, edit_list)		
-		return -1, '', {}
+					print wlist[x]
+					return pos, wlist[x], elist[x]
+			n += 1
+		return -1, read, {}
 
 	def _align(self, read):
 		pos = self._query_fm(read)
 		if pos > -1:
 			edits = self._edit_str({}, read, read)
 		else:
-			wlist = []
-			wlist.append(read)
-			edit_list = {}
-			pos, word, edit_list = self._findNeighbor(wlist, read, read, len(read), 1, 0, edit_list)
+			pos, word, edit_list = self._neighbor_control(read)
 			if pos > -1:
 				edits = self._edit_str(edit_list, word, read)
 			else:
 				edits = list(read)
 
-		return pos, edits
+		return pos, edits #Build a tree instead?
 
 	def _subBase(self, pos, char):
 		#TODO
--- project_code/test_aligner.py
+++ project_code/test_aligner.py
@@ -8,20 +8,29 @@ a = Aligner(string)
 #test insertion
 pos, edits = a._align("ACTTGC")
 print pos, edits
-#assert pos == 0
+assert pos == 0
 #assert edits == "MMMIMM"
 
 #test replacement
 pos, edits = a._align("TACTT")
 print pos, edits
-#assert pos == 4
+assert pos == 4
 #assert edits == "MRMMM"
 
 #test deletion
 pos, edits = a._align("TCTTT")
 print pos, edits
-#assert pos == 6
+assert pos == 6
 #assert edits == "DMMMM"
+
+pos, edits = a._align("AAA")
+print pos, edits
+assert pos == 9
 
 pos, edits = a._align("TCTGTTTC")
 print pos, edits
+assert pos == 2
+
+pos, edits = a._align("AAAA")
+print pos, edits
+assert pos == 9
