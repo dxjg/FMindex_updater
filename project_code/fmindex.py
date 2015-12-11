84bac820a50d2be835d2b626913c916b96dbb779
David Gong <dgong2@outlook.com>
2015-12-11 15:11:56 -0500

    Created base code for naive rank count for comparison based analysis

--- project_code/fmindex.py
+++ project_code/fmindex.py
@@ -17,13 +17,13 @@ class FMindex(object):
     def getInverse(self):
         #Find the row in L with '$'. We'll LF backwards to get the genome
         result = []
-        lf = 0 
+        lf = 0
         while self.bwt[lf] != '$':
             result.append(self.bwt[lf])
             lf = self._lf(lf)
         return ''.join(reversed(result))
 
-        
+
     #Perform the standard LF mapping
     def _lf(self, i):
         #if in the middle of editing, use the computed lf value
@@ -61,7 +61,7 @@ class FMindex(object):
     #Return the 1-based index of ranks
     def rank(self, c, i):
         #The minus 1 seems to be necessary because the implemented
-        #rank function is 1 limited. So the first occurance has value 1 
+        #rank function is 1 limited. So the first occurance has value 1
         result = self.wave.rank(c, i) - 1
         if self.state == "DELETING" and self.pos_first_modif < i and self.overwritten == c:
             result -= 1
@@ -97,7 +97,7 @@ class FMindex(object):
             else:
                 print self.sa[i], self.isa[i], sorted(self.bwt)[i], self.bwt[i]
 
-       
+
 
 ################################################################################
 #                           "Private" Methods Below                            #
@@ -124,7 +124,7 @@ class FMindex(object):
         for i in range(len(sa)):
             isa.append(sa.index(i))
         return sa, isa
-    
+
     #Find the row corresponding to the ith rotation
     def _index(self, i):
         return self.isa[i]
@@ -139,7 +139,7 @@ class FMindex(object):
             if self.isa[x] >= k_prime:
                 self.isa[x] += 1
         self.isa.insert(i, k_prime)
-        
+
     def printRank(self):
         print '_|' + '|'.join(list(self.bwt))
         for letter in sorted(set(list(self.bwt))):
@@ -159,7 +159,7 @@ class FMindex(object):
         temp = self.sa[j]
         self.sa[j] = self.sa[j_prime]
         self.sa[j_prime] = temp
-        
+
         #Updating ISA
         for value in range(j + 1, j_prime + 1):
             index = self.isa.index(value)
@@ -186,7 +186,7 @@ class FMindex(object):
     def delete(self, i):
         self.bwt = self.bwt[:i] + self.bwt[i + 1:]
         self.wave.delete(i)
-        
+
         if self.state in ["INSERTING", "DELETING", "SUBSTITUTING"]:
             #post delete modifications
             if self.lf_before >= i:
@@ -204,10 +204,10 @@ class FMindex(object):
 
         #the character to be inserted
         self.c = c
-        
+
         #Find the index of the ith sorted cyclic shift
         self.pos_first_modif = self.isa[i]
-        
+
         #Needed by lf_after later, since pos_first_modif can be changed by then
         k = self.pos_first_modif
 
@@ -215,32 +215,32 @@ class FMindex(object):
 
         #The character to be overwritten by Stage Ib
         self.overwritten = self.bwt[self.pos_first_modif]
-        
+
         #Rank of the overwritten character, used in both lf_before after the sub
         rank = self.rank(self.overwritten, self.pos_first_modif)
 
-        print "Overwriting the character in L at index", self.pos_first_modif        
+        print "Overwriting the character in L at index", self.pos_first_modif
         #Perform the substitution of overwritten in L with c
         self.delete(self.pos_first_modif)
         self.insert(c, self.pos_first_modif)
         #Changes state of the algorithm. impacts behavior of self.insert and self.delete
         self.state = "INSERTING"
-        
+
         #The LF of the overwritten character before being replaced
         self.lf_before = self.count(self.overwritten) + rank
-        
+
         #Modify this LF based on the qualities of the inserted character
         if self.overwritten > self.c:
             self.lf_before -= 1
             print "decreasing lf_before because overwritten > c:", self.lf_before
-        
+
         #The LF of the newly subbed in character (may be different than the _before one)
         self.lf_after = self.count(self.c) + self.rank(self.c, k)
         print "lf_before:", self.lf_before, "lf_after:", self.lf_after
 
-    def ins_stageIIa(self, i): 
-        #The letter being subbed in heavily impacts the location of insert 
-        print "inserting", self.overwritten, "at row:", self.lf_after 
+    def ins_stageIIa(self, i):
+        #The letter being subbed in heavily impacts the location of insert
+        print "inserting", self.overwritten, "at row:", self.lf_after
         #Perform the insertion of the row corresponding to c in F and overwritten in L
         #code has direct manipulation, not this insert call?
         #self.insert(self.overwritten, self.lf_after)
@@ -251,7 +251,7 @@ class FMindex(object):
         self._updateSA(self.lf_after, i)
 
 
-    def ins_stageIIb(self, i): 
+    def ins_stageIIb(self, i):
         #Prepare the two LFs for stage IIb
         #This handles most of the effects varying i and c can have on the process
         print "Current state of before/after:", self.lf_before, self.lf_after
@@ -260,14 +260,14 @@ class FMindex(object):
             print "increasing lf_before because after is <=:", self.lf_before
         if self.lf_after <= self.pos_first_modif:
             self.pos_first_modif += 1
-       
+
         self.state = "REORDERING"
         #store the letter currently in the lf_before slot (the new character)
         self.L_store = self.overwritten
-        
+
         #the count for this letter
         smaller = self.count(self.L_store)
-        
+
         #self.lf_before = i -1, corresponding to the T[i-1] cyclic shift
 
         #expected/computed index of T'[i-1] self.lf_before' in algorithm
@@ -277,26 +277,26 @@ class FMindex(object):
             #Store temporary values needed throughout the loop
             self.L_store = self.bwt[self.lf_before]
             smaller = self.count(self.L_store)
-            temp_lf_before = smaller + self.rank(self.L_store, self.lf_before) 
+            temp_lf_before = smaller + self.rank(self.L_store, self.lf_before)
             print "Moving rows" , self.lf_before, expected
             print "which affects", self.L_store, self.bwt[expected]
-            self._move(self.lf_before, expected)            
+            self._move(self.lf_before, expected)
 
             #Perform the round of LFs to progress to next while
             self.lf_after = expected
             self.lf_before = temp_lf_before
-            expected = self._lf(self.lf_after)
+            expected = smaller + self.rank(self.L_store, self.lf_after)
             print "Loop of Step IIb"
-            print "j =", self.lf_before, "j_prime =", expected 
+            print "j =", self.lf_before, "j_prime =", expected
         #end while
         self.state = "IDLE"
         self.lf_after = expected
-        
+
 
 ####################################################################################
 #                              Deletion of 1 element                               #
 ####################################################################################
-        
+
 
     '''Stage Ib. Given the deletion index i, overwrite the character at Ti with Ti-1'''
     def  del_stageIb(self, i):
@@ -327,5 +327,5 @@ class FMindex(object):
             self.count = self._createCount()
             self.occ = self._createOcc()
             self.lf_after = self._lf(self.lf_after)
-        
+
 
